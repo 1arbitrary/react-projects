2d980022be23df8b258a056bc7c814916c73b125
dot <rawatkushagra33@gmail.com>
2025-12-20 15:36:30 +0530

    Updated Favicon and html tags

--- Assembly/rsbuild.config.ts
+++ Assembly/rsbuild.config.ts
@@ -3,5 +3,15 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
+  html: {
+    favicon: './src/favicon.jpg',
+    title: 'Assembly: Endgame ',
+    meta: {
+      charset: {
+        charset: 'UTF-8',
+      },
+      viewport: 'width=device-width, initial-scale=1.0',
+    },
+  },
   plugins: [pluginReact()],
 });
